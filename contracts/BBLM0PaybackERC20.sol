3e9a216731772e2f1f0cc20c130cdbf1e40d1e38
szverev <sergey@trood.com>
2022-11-27 22:25:39 +0400

    Update and rename TRST0PaybackERC20.sol to BBLM0PaybackERC20.sol

--- contracts/BBLM0PaybackERC20.sol
+++ contracts/BBLM0PaybackERC20.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TRST0PaybackERC20 is Ownable {
+contract BBLM0PaybackERC20 is Ownable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
